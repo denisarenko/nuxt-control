40ac887b5274aa4d54323b0d27c4c6fac83ac680
denisarenko <denisarenko@gmail.com>
2024-04-26 17:40:32 +0300

    Update Control Components

--- components/control/ControlCard.vue
+++ components/control/ControlCard.vue
@@ -12,11 +12,7 @@
         target="_blank"
         class="text-slate-400 hover:text-blue-500 duration-200 ml-auto mb-auto"
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 448 512"
-          class="size-5"
-        >
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="size-5">
           <path
             fill="currentColor"
             d="M384 64c17.7 0 32 14.3 32 32V416c0 17.7-14.3 32-32 32H64c-17.7 0-32-14.3-32-32V96c0-17.7 14.3-32 32-32H384zm64 32c0-35.3-28.7-64-64-64H64C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V96zM168 160c-8.8 0-16 7.2-16 16s7.2 16 16 16h97.4L132.7 324.7c-6.2 6.2-6.2 16.4 0 22.6s16.4 6.2 22.6 0L288 214.6V320c0 8.8 7.2 16 16 16s16-7.2 16-16V176c0-8.8-7.2-16-16-16H168z"
@@ -25,22 +21,15 @@
       </NuxtLink>
     </div>
 
-    <div class="p-3 flex-1 truncate" :title>
+    <div class="p-3 flex-1 font-medium truncate" :title>
       {{ title }}
     </div>
 
     <div class="control-details p-3 flex flex-wrap gap-3">
       <slot />
 
-      <div
-        v-if="hidden"
-        class="bg-orange-50 flex items-center gap-1 text-orange-700 shadow ring-orange-200"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 640 512"
-          class="size-4"
-        >
+      <div v-if="hidden" class="bg-orange-50 flex items-center gap-1 text-orange-700 shadow ring-orange-200">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" class="size-4">
           <path
             fill="currentColor"
             d="M38.8 5.1C28.4-3.1 13.3-1.2 5.1 9.2S-1.2 34.7 9.2 42.9l592 464c10.4 8.2 25.5 6.3 33.7-4.1s6.3-25.5-4.1-33.7L525.6 386.7c39.6-40.6 66.4-86.1 79.9-118.4c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C465.5 68.8 400.8 32 320 32c-68.2 0-125 26.3-169.3 60.8L38.8 5.1zm151 118.3C226 97.7 269.5 80 320 80c65.2 0 118.8 29.6 159.9 67.7C518.4 183.5 545 226 558.6 256c-12.6 28-36.6 66.8-70.9 100.9l-53.8-42.2c9.1-17.6 14.2-37.5 14.2-58.7c0-70.7-57.3-128-128-128c-32.2 0-61.7 11.9-84.2 31.5l-46.1-36.1zM394.9 284.2l-81.5-63.9c4.2-8.5 6.6-18.2 6.6-28.3c0-5.5-.7-10.9-2-16c.7 0 1.3 0 2 0c44.2 0 80 35.8 80 80c0 9.9-1.8 19.4-5.1 28.2zm51.3 163.3l-41.9-33C378.8 425.4 350.7 432 320 432c-65.2 0-118.8-29.6-159.9-67.7C121.6 328.5 95 286 81.4 256c8.3-18.4 21.5-41.5 39.4-64.8L83.1 161.5C60.3 191.2 44 220.8 34.5 243.7c-3.3 7.9-3.3 16.7 0 24.6c14.9 35.7 46.2 87.7 93 131.1C174.5 443.2 239.2 480 320 480c47.8 0 89.9-12.9 126.2-32.5zm-88-69.3L302 334c-23.5-5.4-43.1-21.2-53.7-42.3l-56.1-44.2c-.2 2.8-.3 5.6-.3 8.5c0 70.7 57.3 128 128 128c13.3 0 26.1-2 38.2-5.8z"
@@ -56,15 +45,9 @@
       class="p-3 flex gap-3 *:flex-1 *:bg-white *:shadow *:ring-1 *:ring-slate-200 *:rounded-md *:p-1 *:text-sm text-center"
     >
       <slot name="actions">
-        <NuxtLink :to="actionEdit" class="hover:bg-slate-50 duration-200">
-          Edit
-        </NuxtLink>
+        <NuxtLink :to="actionEdit" class="hover:bg-slate-50 duration-200"> Edit </NuxtLink>
 
-        <button
-          type="button"
-          @click="actionDelete"
-          class="hover:bg-slate-50 hover:text-red-500 duration-200"
-        >
+        <button type="button" @click="actionDelete" class="hover:bg-slate-50 hover:text-red-500 duration-200">
           Delete
         </button>
       </slot>
@@ -78,21 +61,21 @@ defineProps({
   title: {
     type: String,
     required: true,
-    default: "No Title",
+    default: 'No Title'
   },
   image: {
-    type: String,
+    type: String
   },
   open: {
     type: String,
-    default: null,
+    default: null
   },
   actionEdit: {
-    type: String,
+    type: String
   },
   actionDelete: {
-    type: Function,
-  },
+    type: Function
+  }
 });
 </script>
 
--- components/control/ControlLayout.vue
+++ components/control/ControlLayout.vue
@@ -13,16 +13,8 @@
         <div v-if="hasAuth" class="flex gap-5 items-center">
           {{ accountData.name }}
 
-          <button
-            type="button"
-            @click="logout"
-            class="bg-white ring-1 ring-slate-200 rounded-full size-9 flex shadow"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 512 512"
-              class="size-4 m-auto"
-            >
+          <button type="button" @click="logout" class="bg-white ring-1 ring-slate-200 rounded-full size-9 flex shadow">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="size-4 m-auto">
               <path
                 d="M256 56a24 24 0 1 1 48 0 24 24 0 1 1 -48 0zm80 0A56 56 0 1 0 224 56a56 56 0 1 0 112 0zM179.4 116.4c-12.7-3.2-26.1-3.2-38.8 0l-1.5 .4c-12.3 3.1-23.7 9-33.3 17.4L53.5 180c-6.7 5.8-7.3 15.9-1.5 22.6s15.9 7.3 22.6 1.5l52.4-45.8c5.7-5 12.6-8.6 20-10.4l1.5-.4c7.6-1.9 15.6-1.9 23.3 0l16.9 4.2L153.2 257.9c-7.3 21.8 2 45.7 22 57l93.3 52.5L224.9 490.7c-2.9 8.3 1.4 17.5 9.8 20.4s17.5-1.4 20.4-9.8L298.6 378c5.2-14.7-.9-30.9-14.5-38.5l-37.5-21.1c.2-.4 .4-.9 .5-1.3l37.2-111.6L298 253.2c5.9 20.6 24.7 34.8 46.2 34.8H400c8.8 0 16-7.2 16-16s-7.2-16-16-16H344.1c-7.1 0-13.4-4.7-15.4-11.6l-20.3-71c-4.7-16.5-17.9-29.2-34.5-33.4l-94.5-23.6zm83 53.7L218.3 302.4 190.9 287c-6.7-3.8-9.8-11.7-7.3-19l36.2-108.6 42.7 10.7zM130 314.1L101.3 384H16c-8.8 0-16 7.2-16 16s7.2 16 16 16h85.3c13 0 24.7-7.8 29.6-19.8l23.3-56.6c-10-6.7-18.2-15.5-24.1-25.4zM458.5 444.2C445.6 480 433.6 480 432 480h0 0c-1.6 0-13.6 0-26.4-35.8c-9.4-26.3-16.8-66.4-19.9-124.2h-32c7.3 140.7 39.7 192 78.4 192c44.2 0 80-66.6 80-256S476.2 0 432 0c-41.6 0-75.8 59-79.6 224h32c1.8-75.3 10.1-125.3 21.2-156.2C418.4 32 430.4 32 432 32l0 0 0 0c1.6 0 13.6 0 26.4 35.8C471.1 102.9 480 162.5 480 256s-8.9 153.1-21.5 188.2z"
               />
@@ -31,26 +23,19 @@
         </div>
       </header>
 
-      <main
-        v-if="hasAuth"
-        class="grid grid-cols-[250px_1fr] items-start gap-8 pb-8"
-      >
-        <aside
-          class="bg-white ring-1 ring-slate-200 shadow grid gap-2 rounded-3xl p-4 sticky top-5"
-        >
+      <main v-if="hasAuth" class="grid grid-cols-[250px_1fr] items-start gap-8 pb-8">
+        <aside class="bg-white ring-1 ring-slate-200 shadow grid gap-2 rounded-3xl p-4 sticky top-5">
           <template v-for="menu in menuLinks" :key="menu">
             <NuxtLink
               v-if="!menu.divide"
               :to="menu.link"
-              class="px-4 py-2 hover:bg-slate-100 rounded-lg duration-200"
+              active-class="ring-1 ring-slate-200 shadow"
+              class="px-4 py-2 hover:bg-slate-50 rounded-lg duration-200"
             >
               {{ menu.name }}
             </NuxtLink>
 
-            <div
-              v-else
-              class="uppercase text-xs px-4 font-semibold opacity-50 my-2"
-            >
+            <div v-else class="uppercase text-xs px-4 font-semibold opacity-50 my-2">
               {{ menu.name }}
             </div>
           </template>
@@ -59,10 +44,7 @@
         <slot />
       </main>
 
-      <section
-        v-else
-        class="bg-white ring-1 ring-slate-200 shadow rounded-3xl p-5 md:py-14"
-      >
+      <section v-else class="bg-white ring-1 ring-slate-200 shadow rounded-3xl p-5 md:py-14">
         <slot name="auth" />
       </section>
     </div>
@@ -73,26 +55,22 @@
 defineProps({
   menuLinks: {
     type: Array,
-    required: true,
+    required: true
   },
   logout: {
     type: Function,
-    required: true,
+    required: true
   },
   hasAuth: Boolean,
   accountData: {
-    type: Object,
-  },
+    type: Object
+  }
 });
 </script>
 
 <style>
 .bg-dot {
-  background-image: radial-gradient(
-    circle at 2px 2px,
-    theme("colors.slate.200") 1px,
-    transparent 0
-  );
+  background-image: radial-gradient(circle at 2px 2px, theme('colors.slate.200') 1px, transparent 0);
   background-size: 16px 16px;
 }
 </style>
